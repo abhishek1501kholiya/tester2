2908b9b441fa72f73259295bb56d8ba90ae321a3
abhishek1501kholiya <64292097+abhishek1501kholiya@users.noreply.github.com>
2020-08-27 14:36:23 +0530

    Add files via upload

---
+++ My.js
@@ -0,0 +1,22 @@
+var provider = new  firebase.auth.EmailAuthProvider();
+function signIn() {
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+	  // This gives you a Google Access Token. You can use it to access the Google API.
+	  var token = result.user.uid;
+	  // The signed-in user info.
+	  user = result.user;
+	  showWelcomeContainer();
+	  sessionStorage.token = token;
+	  // ...
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+	});
+
+};
